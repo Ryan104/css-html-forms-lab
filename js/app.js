7b8aad8ed9dd281f2e3624b687baf458d7add73e
Ryan <ryan.d.elliott@gmail.com>
2017-09-19 16:48:25 -0600

    Form data posts to page

--- js/app.js
+++ js/app.js
@@ -2,10 +2,16 @@ $(document).ready(function(){
 	console.log('document ready!');
 
 	var main = $('#main');
+	$( "<section class='container results'></section>" ).appendTo(main);
+	var $results = $('.results');
+
 
 	function makeResults() {
+		// Clear any old content
+		$results.empty();
 
-		$( "<section class='container results'><h1>A True Story:</h1>" ).appendTo(main);
+		// Append story header
+		$results.append('<h1>A True Story:</h1>');
 
 		// Get all form data
 		const madLibs = {
@@ -18,10 +24,22 @@ $(document).ready(function(){
 				.map(function(){return this.value})
 				.get().join(", "),
 			noun2: $('input[name=noun-2]').val()
-
 		};
 
 		console.log(madLibs);
+
+		// Build output string.
+
+		const resultsText = `
+			Once upon a time there was a Developer named ${madLibs.name}.
+			${madLibs.name} came to General Asssembly to ${madLibs.verb1} the great mountain of programming knowledge.
+			"${madLibs.mission}", ${madLibs.name} shouted.
+			Though ${madLibs.sentiment}, ${madLibs.name} was able to overcome all obstacles with the power of their ${madLibs.noun1}.
+			In the end, their success can be contributed entirely to their ${madLibs.adjectives} ${madLibs.noun2}.
+		`;
+
+		console.log(resultsText);
+		$('.results').append('<p>' + resultsText);
 
 	};
 
