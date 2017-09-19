120a5668ce36ea9e83a1e13b843a3a0800bc3c10
Ryan <ryan.d.elliott@gmail.com>
2017-09-19 16:05:12 -0600

    Update makeResults() to collect data from form

--- js/app.js
+++ js/app.js
@@ -1,23 +1,37 @@
 $(document).ready(function(){
 	console.log('document ready!');
+
 	var main = $('#main');
 
 	function makeResults() {
 
 		$( "<section class='container results'><h1>A True Story:</h1>" ).appendTo(main);
 
-		// JS Native:
-		// var results = document.createElement('section');
-		// results.innerHTML = '<h1>' + 'A true story:' + '</h1>';
-		// results.className = 'container results';
-		// main.appendChild(results);
+		// Get all form data
+		const madLibs = {
+			name: $('input[name=name]').val(),
+			verb1: $('select[name=verb-1').val(),
+			mission: $('textarea[name=mission]').val(),
+			sentiment: $('input[name=sentiment]:checked').val(),
+			noun1: $('input[name=noun-1]').val(),
+			adjectives: $('input[name=adjective-2]:checked')
+				.map(function(){return this.value})
+				.get().join(", "),
+			noun2: $('input[name=noun-2]').val()
+
+		};
+
+		console.log(madLibs);
+
 	};
 
 	$("form").on("submit", function(event){
 
 	    // Stop the form from submitting
+	    event.preventDefault();
 
 		// Run the 'makeResults' function seen above
+		makeResults();
 
 		/*
 		
